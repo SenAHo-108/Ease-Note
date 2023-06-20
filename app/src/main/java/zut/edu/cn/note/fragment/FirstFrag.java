81f65f95d80dfc6c17e17b02af8f4d42a532961c
âSunflower <1955731619@qq.com>
2023-06-20 16:31:37 +0800

    aaaaa

--- app/src/main/java/zut/edu/cn/note/fragment/FirstFrag.java
+++ app/src/main/java/zut/edu/cn/note/fragment/FirstFrag.java
@@ -65,6 +65,7 @@ public class FirstFrag extends Fragment {
     }
 
     private void init() {
+        System.out.println("111111");
         gv = view.findViewById(R.id.shouye_lv);
         btn_add = view.findViewById(R.id.shouye_btn_add);
         username = getActivity().getIntent().getStringExtra("username");  //得到用户的username
